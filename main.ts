848ffa04abc16ef8cd895d04043626bcacf51002
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-10-22 11:00:11 -0400

    fixed comments

--- main.ts
+++ main.ts
@@ -10,28 +10,34 @@ let numbertwo = randint(0, 99)
 
 basic.showIcon(IconNames.Happy)
 
+//Shows random number one
 input.onButtonPressed(Button.A, function() {
     basic.clearScreen()
     basic.showString('#1:')
     basic.showNumber(numberone)
+    basic.showIcon(IconNames.Happy)
 })
 
+//Shows random number two
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     basic.showString('#2:')
     basic.showNumber(numbertwo)
+    basic.showIcon(IconNames.Happy)
 })
 
 input.onGesture(Gesture.Shake, function() {
-    
+    //checks if number one is greater than number two
     if (numberone>numbertwo) {
         basic.clearScreen()
+        //shows that number one is greater than number two
         basic.showNumber(numberone)
         basic.showString('>')
         basic.showNumber(numbertwo)
         basic.showIcon(IconNames.Sad)
     } else{
         basic.clearScreen()
+        //shows that number two is greater than number one
         basic.showNumber(numberone)
         basic.showString('<')
         basic.showNumber(numbertwo)
