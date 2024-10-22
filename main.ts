ecd768acfb15b680ee206c552d853f47b0ae90fa
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-10-22 10:55:19 -0400

    compares two random numbers

--- main.ts
+++ main.ts
@@ -1,8 +1,39 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Joos
+ * Created on: Oct 2024
+ * This program compares two random numbers
 */
 
-basic.showString('Hello, World!')
+let numberone = randint (0, 99)
+let numbertwo = randint(0, 99)
+
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    basic.showString('#1:')
+    basic.showNumber(numberone)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString('#2:')
+    basic.showNumber(numbertwo)
+})
+
+input.onGesture(Gesture.Shake, function() {
+    
+    if (numberone>numbertwo) {
+        basic.clearScreen()
+        basic.showNumber(numberone)
+        basic.showString('>')
+        basic.showNumber(numbertwo)
+        basic.showIcon(IconNames.Sad)
+    } else{
+        basic.clearScreen()
+        basic.showNumber(numberone)
+        basic.showString('<')
+        basic.showNumber(numbertwo)
+        basic.showIcon(IconNames.Sad)
+}})
